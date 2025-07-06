59ac2ce3ecd811b5feceea359159f7b214f92d6e
Juhanbin <gksqls0509@naver.com>
2025-07-06 17:57:56 +0900

    쉐이더 클래스 메모

--- Engine/Private/Shader.cpp
+++ Engine/Private/Shader.cpp
@@ -33,23 +33,38 @@ HRESULT CShader::Initialize_Prototype(const _tchar* pShaderFilePath, const D3D11
 	if (FAILED(D3DX11CompileEffectFromFile(pShaderFilePath, nullptr, D3D_COMPILE_STANDARD_FILE_INCLUDE, iHlslFlag, 0, m_pDevice, &m_pEffect, nullptr)))
 		return E_FAIL;
 
+	//이펙트 객체에서 "첫 번째 테크닉"을 얻음
 	ID3DX11EffectTechnique* pTechnique = m_pEffect->GetTechniqueByIndex(0);
 	if (nullptr == pTechnique)
 		return E_FAIL;
 
+	//테크닉의 속성(설명)을 얻어옴
 	D3DX11_TECHNIQUE_DESC	TechniqueDesc{};
 	pTechnique->GetDesc(&TechniqueDesc);
 
+	//그 테크닉이 가진 패스 개수(Passes)를 m_iNumPasses에 저장
 	m_iNumPasses = TechniqueDesc.Passes;
 
 	for (size_t i = 0; i < m_iNumPasses; i++)
 	{
+		//각 패스 객체를 하나씩 꺼냄
 		ID3DX11EffectPass* pPass = pTechnique->GetPassByIndex(i);
 		if (nullptr == pPass)
 			return E_FAIL;		
 
+		//패스의 속성(설명)을 PassDesc에 저장
 		D3DX11_PASS_DESC	PassDesc{};
 		pPass->GetDesc(&PassDesc);
+
+		// InputLayout(인풋 레이아웃)이란?
+		//정점 버퍼에 들어 있는 데이터(예: POSITION, NORMAL, TEXCOORD 등)**가
+		// 메모리에 어떻게 저장되어 있는지, 그리고 각각의 의미가 뭔지 GPU / 셰이더에 "정확히 설명"해주는 객체
+
+		//Elements(엘리먼츠)란?
+		//정점 1개당, 어떤 속성이 어떤 순서/위치에 저장돼 있는지 각각 "엘리먼트" 단위로 정의하는 것.
+		//D3D11_INPUT_ELEMENT_DESC Elements[] = {
+		//{ "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+		//{ "TEXCOORD", 0, DXGI_FORMAT_R32G32_FLOAT,    0, 12, D3D11_INPUT_PER_VERTEX_DATA, 0 },
 
 		ID3D11InputLayout* pInputLayout = { nullptr };
 
@@ -74,7 +89,8 @@ HRESULT CShader::Begin(_uint iPassIndex)
 
 	if (FAILED(m_pEffect->GetTechniqueByIndex(0)->GetPassByIndex(iPassIndex)->Apply(0, m_pContext)))
 		return E_FAIL;
-
+	
+	//패스에 맞는 InputLayout을 Input Assembler(IA)에 바인딩
 	m_pContext->IASetInputLayout(m_InputLayouts[iPassIndex]);
 
 	return S_OK;
@@ -84,15 +100,18 @@ HRESULT CShader::Begin(_uint iPassIndex)
 
 HRESULT CShader::Bind_Matrix(const _char* pConstantName, const _float4x4* pMatrix)
 {
-	ID3DX11EffectVariable*	pVariable = m_pEffect->GetVariableByName(pConstantName);
+	//셰이더에서 pConstantName(예: "g_WorldMatrix")라는 이름의 변수(상수버퍼 멤버)를 찾음
+	ID3DX11EffectVariable* pVariable = m_pEffect->GetVariableByName(pConstantName);
 	if (nullptr == pVariable)
 		return E_FAIL;
 
+	//찾은 변수를 "행렬 타입"으로 캐스팅(변환)
 	ID3DX11EffectMatrixVariable* pMatrixVariable = pVariable->AsMatrix();
 	if (nullptr == pMatrixVariable)
 		return E_FAIL;
 
-	return pMatrixVariable->SetMatrix(reinterpret_cast<const _float*>(pMatrix));	
+	//실제 C++의 행렬 포인터(pMatrix, float4x4 구조체)**를 셰이더에 전달
+	return pMatrixVariable->SetMatrix(reinterpret_cast<const _float*>(pMatrix));
 }
 
 HRESULT CShader::Bind_SRV(const _char* pConstantName, ID3D11ShaderResourceView* pSRV)
