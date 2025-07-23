1ecc7f984a35958976e3645e8e29b91b8d26bf8a
Juhanbin <gksqls0509@naver.com>
2025-07-23 10:29:00 +0900

    레이

--- example_win32_directx11/Public/MainApp.h
+++ example_win32_directx11/Public/MainApp.h
@@ -3,54 +3,66 @@
 #include "Edit_Defines.h"
 #include "Base.h"
 
+#include <DirectXMath.h>
+#include <DirectXCollision.h>
+#include <vector>
+
 NS_BEGIN(Engine)
 class CGameInstance;
 NS_END
+
+struct Ray {
+    DirectX::XMFLOAT3 origin;
+    DirectX::XMFLOAT3 dir;
+};
 
 NS_BEGIN(Edit)
 
 class CMainApp final : public CBase
 {
 private:
-	CMainApp();
-	virtual ~CMainApp() = default; 
+    CMainApp();
+    virtual ~CMainApp() = default;
 
 public:
-	HRESULT Initialize();
-	void Update(_float fTimeDelta);
-	HRESULT Render();
+    HRESULT Initialize();
+    void Update(_float fTimeDelta);
+    HRESULT Render();
 
 private:
-	CGameInstance*			m_pGameInstance = { nullptr };
-	ID3D11Device*			m_pDevice = { nullptr };
-	ID3D11DeviceContext*	m_pContext = { nullptr };
+    Engine::CGameInstance* m_pGameInstance = nullptr;
+    ID3D11Device* m_pDevice = nullptr;
+    ID3D11DeviceContext* m_pContext = nullptr;
 
 private:
-	HRESULT Ready_Prototype_ForStatic();
-	HRESULT Start_Level(LEVEL eStartLevelID);
+    HRESULT Ready_Prototype_ForStatic();
+    HRESULT Start_Level(LEVEL eStartLevelID);
 
 public:
-	// --- MapTool 관련 함수 ---
-	void Render_ImGuiPanel();
-	void SaveScene(const char* filename);
-	bool LoadScene(const char* filename);
-	void PushUndo();
-	void RefreshScene();
+    // --- MapTool 관련 함수 ---
+    void Render_ImGuiPanel();
+    void SaveScene(const char* filename);
+    bool LoadScene(const char* filename);
+    void PushUndo();
+    void RefreshScene();
+
+    // 피킹 & AABB 교차 함수 선언
+    Ray CreatePickingRay(int mx, int my, int w, int h, const DirectX::XMMATRIX& view, const DirectX::XMMATRIX& proj);
+    bool RayIntersectsAABB(const Ray& ray, const DirectX::BoundingBox& box, float* outDist = nullptr);
 
 private:
-	// --- MapTool 데이터 ---
-	vector<MapObject> m_Objects;
-	vector<vector<MapObject>> m_UndoStack;
-	int m_Selected = -1;
-	float m_TempSize[3] = { 1,1,1 };
-	float m_TempRot[3] = { 0,0,0 };
-	float m_TempPos[3] = { 0,0,0 };
+    std::vector<MapObject> m_Objects;
+    std::vector<std::vector<MapObject>> m_UndoStack;
+    int m_Selected = -1;
+    float m_TempSize[3] = { 1,1,1 };
+    float m_TempRot[3] = { 0,0,0 };
+    float m_TempPos[3] = { 0,0,0 };
 
-	ID3D11RenderTargetView* m_pBackBufferRTV = nullptr;
-	ID3D11DepthStencilView* m_pDepthStencilView = nullptr;
+    ID3D11RenderTargetView* m_pBackBufferRTV = nullptr;
+    ID3D11DepthStencilView* m_pDepthStencilView = nullptr;
 public:
-	static CMainApp* Create();
-	virtual void Free() override;		
+    static CMainApp* Create();
+    virtual void Free() override;
 };
 
 NS_END
